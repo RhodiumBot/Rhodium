72f59e8a47d696c5ae4a0436a50234785175cd72
Ron31 <ron3108@gmx.net>
2018-10-08 14:26:59 +0200

    Updated build targets.

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,11 @@
 const { connection } = require('../utils/database');
 module.exports = async (client) => {
     console.log(`Started Up!`);
-    try {
+    /*try {
         connection.sync();
         console.log('Synced!')
     } catch(e) {
         console.log(e);
-    }
+    }*/
     //status loop?
 };
