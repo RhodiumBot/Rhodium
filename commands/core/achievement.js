7068c16d203cf75d720b1a39a03d7641878b28eb
Ron31 <ron3108@gmx.net>
2018-10-13 09:35:08 +0200

    Update achievement.js

--- commands/core/achievement.js
+++ commands/core/achievement.js
@@ -1,12 +1,13 @@
 const Discord   = require('discord.js');
 const snekfetch = require('snekfetch');
 module.exports.run = async (msg, args, client) => {
+    let picture;
     if(msg.mentions.members.first()) {
-        let picture = msg.mentions.users.first().avatarURL+"?size=128";
+        picture = msg.mentions.users.first().avatarURL+"?size=128";
         let text = await args.join(" ").replace(`<@!${msg.mentions.members.first().id}>`, "").replace(`<@${msg.mentions.members.first().id}>`, "")
     } else {
         let text = args.join(" ");
-        let picture = "https://cdn.discordapp.com/emojis/495931857620434944.png?v1";
+        picture = "https://cdn.discordapp.com/emojis/495931857620434944.png?v1";
     }
     let res = await snekfetch.post(`http://localhost:8081/api/image/achievement`)
         .send({ "image": picture, "token": "Not_Used_here", "achievement": text});
