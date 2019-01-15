4f08c1a9e868de9ec612d627760c593d5eafc744
Stu <justuskliem@gmail.com>
2019-01-15 15:38:29 +0100

    Improved Token Protection

--- commands/vserver/eval.js
+++ commands/vserver/eval.js
@@ -14,11 +14,10 @@ module.exports.run = async (msg, args, client) => {
     console.log(befehl)
     try {
         let ergebniss = await eval(befehl)
-        if(!silent)client.embed.success(msg.channel, '```' + ergebniss + '```', '<a:success:471096765613998080> Eval finished successfully.')
+        ergebniss.replace(client.config.token, "<Censored Token>")
+
+        if(!silent) client.embed.success(msg.channel, '```' + ergebniss + '```', '<a:success:471096765613998080> Eval finished successfully.')
         else return;
-        if (typeof ergebniss == "string" && ergebniss.includes(client.config.token)) {
-            return client.embed.error(msg.channel, '```not today```', ':no_entry_sign: The token is blacklisted!');
-        }
     } catch(e) {
         return client.embed.error(msg.channel, '```' + e.message + '```', ':warning: Eval returned an error');
     }
