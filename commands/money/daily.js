ddaad3379d3463f3f61f0386fafba72b15e4b9c7
rmc_productions <r.vleug@gmail.com>
2019-02-27 19:46:11 +0100

    better daily algorithm

--- commands/money/daily.js
+++ commands/money/daily.js
@@ -5,7 +5,7 @@ module.exports.run = async (msg, args, client) => {
     let entry = await user.findOne({ where: { user: msg.author.id}});
     if(!entry) return msg.channel.send("Error! Please use >>devmsg for help.");
     if(Date.now() - entry.lastclaimed <= 86400000) return msg.channel.send(`Next daily in: ${Math.round((entry.lastclaimed - Date.now() + 86400000 )/ 60000)}min (${Math.round((entry.lastclaimed - Date.now() + 86400000 )/ 60000 / 60)}h)`)
-    let money = Math.round(Math.random() * 100);
+    let money = 50 + Math.round(Math.random() * 50);
     await user.update({ credits: entry.credits + money, lastclaimed: Date.now() }, { where: { user: msg.author.id }});
     msg.channel.send(`Your Daily Credits: ${money}`)
 };
