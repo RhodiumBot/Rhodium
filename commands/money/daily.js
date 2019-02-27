5b4b5401c67941821f163c6da39511b17dcdfb22
Ron31 <ron3108@gmx.net>
2019-02-27 19:15:17 +0100

    Update daily.js

--- commands/money/daily.js
+++ commands/money/daily.js
@@ -6,13 +6,13 @@ module.exports.run = async (msg, args, client) => {
     if(!entry) return msg.channel.send("Error! Please use >>devmsg for help.");
     if(entry.lastclaimed + 86400000 >= Date.now()) return msg.channel.send(`Next daily in: ${(entry.lastclaimed + 86400000 - Date.now())/ 60000} min`)
     let money = Math.round(Math.random() * 100);
-    await user.update({ credits: entry.credits + 10, lastclaimed: Date.now() }, { where: { user: msg.author.id }});
+    await user.update({ credits: entry.credits + money, lastclaimed: Date.now() }, { where: { user: msg.author.id }});
     msg.channel.send(`Your Daily Credits: ${money}`)
 };
 
 exports.info = {
-    description: "Gives you a random amount of money",
-    level: 1,
-    enabled: false,
+    description: "Gives you a random amount of money [Testing]",
+    level: 6,
+    enabled: true,
     usage: [" "]
 };
