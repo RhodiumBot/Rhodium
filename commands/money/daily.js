1b734a481eca55d26052ef8899e3f564102d2f14
rmc_productions <r.vleug@gmail.com>
2019-02-28 19:37:32 +0100

    daily counter update

--- commands/money/daily.js
+++ commands/money/daily.js
@@ -4,7 +4,8 @@ module.exports.run = async (msg, args, client) => {
     if(args.length < 0 ) return msg.channel.send("Usage: `>>daily`");
     let entry = await user.findOne({ where: { user: msg.author.id}});
     if(!entry) return msg.channel.send("Error! Please use >>devmsg for help.");
-    if(Date.now() - entry.lastclaimed <= 86400000) return msg.channel.send(`Next daily in: ${Math.round((entry.lastclaimed - Date.now() + 86400000 )/ 60000)}min (${Math.round((entry.lastclaimed - Date.now() + 86400000 )/ 60000 / 60)}h)`)
+    let d = new Date((Date.now() - 86400000 - entry.lastclaimed) / -1);
+    if(Date.now() - entry.lastclaimed <= 86400000) return msg.channel.send(`Next daily in: ${d.getHours() - 1}h ${d.getMinutes()}m`)
     let money = 50 + Math.round(Math.random() * 50);
     await user.update({ credits: entry.credits + money, lastclaimed: Date.now() }, { where: { user: msg.author.id }});
     msg.channel.send(`Your Daily Credits: ${money}`)
