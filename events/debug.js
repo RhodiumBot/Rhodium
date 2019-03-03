ef80b90b43d6e4b58806b5200eef6947f7d405c2
Ron31 <ron3108@gmx.net>
2019-03-03 17:51:02 +0100

    Update debug.js

--- events/debug.js
+++ events/debug.js
@@ -1,3 +1,3 @@
 module.exports = async (client, debug) => {
-    if(process.argv.includes("debug")) console.log("[DEBUG] ".yellow + debug)
-}
+    if(process.argv.includes("debug")) { console.log("[DEBUG] ".yellow + debug); }
+};
