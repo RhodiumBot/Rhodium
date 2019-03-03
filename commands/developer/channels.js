cd7c566065fbdf5f887bafb7386c65f32d70602e
Ron31 <ron3108@gmx.net>
2019-03-03 08:24:52 +0100

    Update channels.js

--- commands/developer/channels.js
+++ commands/developer/channels.js
@@ -1,8 +1,8 @@
 const DJS = require("discord.js");
 
 let compare = (a, b) => {
-    if(a.position < b.calculatedPosition) return -1;
-    if(a.position > b.calculatedPosition) return 1;
+    if(a.position < b.calculatedPosition){ return -1; }
+    if(a.position > b.calculatedPosition){ return 1; }
     return 0;
 };
 
@@ -10,20 +10,22 @@ module.exports.run = async (msg, args, client) => {
     if(args.join(" ").includes("-l")){
         let contents = [];
 
-        msg.guild.channels.filter(c => c.type != "category").sort(compare).map(i => i).forEach((channel, index) => {
+        msg.guild.channels.filter((c) => c.type !== "category").sort(compare).map((i) => i).forEach((channel, index) => {
 
             let guildInfo = new DJS.RichEmbed()
                 .setTitle(`${channel.name} *(${channel.id})*`)
-                .setDescription(`${channel.type == "voice" ? `${client.vars.emojiIcons.voice} Voice` : `${client.vars.emojiIcons.text} Text`}\n${channel.nsfw ? `${client.vars.emojiIcons.alert} NSFW` : ``}`)
-                .setFooter(`Page ${index+1}/${msg.guild.channels.filter(c => c.type !== "category").size}`)
+                .setDescription(`${channel.type === "voice" ? `${client.vars.emojiIcons.voice} Voice` : `${client.vars.emojiIcons.text} Text`}\n${channel.nsfw ? `${client.vars.emojiIcons.alert} NSFW` : ""}`)
+                .setFooter(`Page ${index+1}/${msg.guild.channels.filter((c) => c.type !== "category").size}`)
                 .setTimestamp(channel.createdTimestamp)
                 .setColor(0xffc600);
             contents.push(guildInfo);
         });
 
-        client.utils.get("pages").multiContent(msg, contents, client)
+        client.utils.get("pages").multiContent(msg, contents, client);
     }
-    else client.utils.get("pages").singleContent(msg, "List of all channels", `${client.vars.emojiIcons.text} Total Channel Count: ${msg.guild.channels.size}\nFor a detailed list, type ${msg.prefix}channels -l`, msg.guild.channels.filter(c => c.type != "category").sort(compare).map(g => `${(g.type == "text" ? `(T)` : `(V)`)}\t${g.name} *(${g.id})*`).join(`\n`), client)
+    else {
+        client.utils.get("pages").singleContent(msg, "List of all channels", `${client.vars.emojiIcons.text} Total Channel Count: ${msg.guild.channels.size}\nFor a detailed list, type ${msg.prefix}channels -l`, msg.guild.channels.filter((c) => c.type !== "category").sort(compare).map((g) => `${(g.type === "text" ? `(T)` : `(V)`)}\t${g.name} *(${g.id})*`).join("\n"), client)
+    }
 
 };
 
@@ -33,4 +35,4 @@ module.exports.info = {
     level: 1,
     enabled: true,
     usage: ["", "-l"]
-}
+};
