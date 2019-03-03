2d486cb652a78f7a77124f582cf585b1d270b093
Ron31 <ron3108@gmx.net>
2019-03-03 17:31:29 +0100

    Update eval.js

--- commands/vserver/eval.js
+++ commands/vserver/eval.js
@@ -6,24 +6,24 @@ module.exports.run = async (msg, args, client) => {
         return msg.channel.send(':no_entry_sign: No rights!')
     }*/
     let silent = false;
-    if (args[0].toLowerCase() == "-s"){
+    if (args[0].toLowerCase() === "-s"){
         silent = true;
         args.shift();
         msg.delete();
     }
-    console.log(args.join(' '))
+    console.log(args.join(" "));
     try {
-        let evalargs = args.join(' ');
-        evalargs.replace("client.config.token", "<Censored Token>")
-        let evaled = await eval(evalargs)
+        let evalargs = args.join(" ");
+        evalargs.replace("client.config.token", "<Censored Token>");
+        let evaled = await eval(evalargs);
         
-        if(!silent)client.embed.success(msg.channel, '```' + evaled + '```', '<a:success:471096765613998080> Eval finished successfully.')
-        else return;
+        if(!silent) { client.embed.success(msg.channel, "```" + evaled + "```", "<a:success:471096765613998080> Eval finished successfully."); }
+        else { return; }
     } catch(e) {
-        return client.embed.error(msg.channel, '```' + e.message + '```', ':warning: Eval returned an error');
+        return client.embed.error(msg.channel, "```" + e.message + "```", ":warning: Eval returned an error");
     }
     
-}
+};
 
 module.exports.info = {
     description: "Eval Command",
