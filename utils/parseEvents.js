7e77ebf3d82c44d453b344961a87e3ca14c870ab
Ron31 <ron3108@gmx.net>
2019-03-03 18:13:15 +0100

    Update parseEvents.js

--- utils/parseEvents.js
+++ utils/parseEvents.js
@@ -4,7 +4,7 @@ module.exports.run = (client) => {
     console.log("Parsing events");
     fs.readdir("./events", (err, files) => {
         if (err) { return console.error(err); }
-        files.forEach(file => {
+        files.forEach((file) => {
             if (!file.endsWith(".js")) { return; }
             console.log(`- Loading file ${file.split(".")[0]}`);
             const event = require(`../events/${file}`);
