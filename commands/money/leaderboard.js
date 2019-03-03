812125bcb696d61be85ef67d31ff405a52dea030
Ron31 <ron3108@gmx.net>
2019-03-03 13:43:46 +0100

    Update leaderboard.js

--- commands/money/leaderboard.js
+++ commands/money/leaderboard.js
@@ -1,17 +1,17 @@
-const { user } = require('../../utils/database');
+const { user } = require("../../utils/database");
 
 let compare = (a, b) => {
-    if(a.dataValues.credits > b.dataValues.credits) return -1;
-    if(a.dataValues.credits < b.dataValues.credits) return 1;
+    if(a.dataValues.credits > b.dataValues.credits) { return -1; }
+    if(a.dataValues.credits < b.dataValues.credits) { return 1; }
     return 0;
 };
 
 module.exports.run = async (msg, args, client) => {
     let entry = await user.findAll();
-    if(!entry) return msg.channel.send("Error! Please use >>devmsg for help.");
+    if(!entry) { return msg.channel.send("Error! Please use >>devmsg for help."); }
     entry.sort(compare);
-    entry = entry.filter(u => client.users.has(u.dataValues.user));
-    client.embed.uni(msg, "Credits leaderboard", entry.map((u, k) => (k < 10 ? `**${k + 1}.** **${client.users.get(u.dataValues.user).tag}** \`${u.dataValues.credits}C\`` : ``)));
+    entry = entry.filter((u) => client.users.has(u.dataValues.user));
+    client.embed.uni(msg, "Credits leaderboard", entry.map((u, k) => (k < 10 ? `**${k + 1}.** **${client.users.get(u.dataValues.user).tag}** \`${u.dataValues.credits}C\`` : "")));
 };
 
 exports.info = {
