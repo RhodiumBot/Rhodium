7ea1a33f53d6370c47ae8fcecc5ff6c2b2bd9f0f
Ron31 <ron3108@gmx.net>
2019-03-03 17:21:45 +0100

    Update debug.js

--- commands/vserver/debug.js
+++ commands/vserver/debug.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (msg, args, client) => {
-    if(msg.author.id != "224084384054116352") return;
-    if(client.DEBUG = true) return client.DEBUG = false;
-    else return client.DEBUG = true;
+    if(msg.author.id !== "224084384054116352") { return; }
+    if(client.DEBUG === true) { return client.DEBUG = false; }
+    else { return client.DEBUG = true; }
 };
 
 module.exports.info = {
