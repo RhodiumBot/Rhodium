2fed598ca32ed750de0c95b6b00d9cfbbbe991bb
Ron31 <ron3108@gmx.net>
2019-04-19 09:43:51 +0200

    Level Fix + Pay

--- commands/money/pay.js
+++ commands/money/pay.js
@@ -5,14 +5,18 @@ module.exports.run = async (msg, args, client) => {
     if(!userm) { return msg.channel.send('Ping someone')}
     let entry = await user.findOne({ where: { user: userm.id}});
     if(!entry) { return msg.channel.send("Error! Please use >>devmsg for help."); }
+    let entry2 = await user.findOne({ where: { user: msg.author.id }});
+    if(!entry2) { return msg.channel.send("Error(2)! Please use >>devmsg for help."); }
+    if(args[1].startsWith('-')) { return msg.channel.send("Only positive, we don´t steal money.")}
     await user.update({ credits: entry.credits + Number(args[1])}, { where: { user: userm.id }});
+    await user.update({ credits: entry2.credits - Number(args[1])}, { where: { user: msg.author.id }});
     //uni(msg, title, description, contents, color, footer, image, thumbnail, url, addition){
-    client.embed.uni(msg, "Award Credits", `He recieved ${args[1]} credits.`);
+    client.embed.uni(msg, "Pay Credits", `He recieved ${args[1]} credits.`);
 };
 
 exports.info = {
-    description: "Award someone money",
-    level: 6,
+    description: "Pay someone money",
+    level: 1,
     enabled: true,
     usage: ["<@user> <money>"],
     dm: false
